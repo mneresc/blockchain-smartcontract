291315b965023efa502c0bdb44bb75b5ed59f8cc
Neres <mneresc@gmail.com>
2024-02-20 08:58:39 -0300

    feat: add contract with mapping structure

---
+++ 02-smartcontracts/00-hello/BookDatabase.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.12;
+
+contract BooksDatabase {
+    struct Book {
+        string title;
+        uint16 year;
+    }
+
+    uint32 id;
+
+    mapping(uint32 => Book) public books;
+
+    function addBook(Book memory newBook) public {
+        id++;
+        books[id] = newBook;
+    }
+
+    function editBook(uint32 idBook, Book memory newBook) public {
+        Book memory oldBook = books[idBook];
+
+        if (
+            !compare(oldBook.title, newBook.title) &&
+            !compare(oldBook.title, "")
+        ) {
+            oldBook.title = newBook.title;
+        }
+
+        if (newBook.year != 0 && oldBook.year != newBook.year) {
+            oldBook.year = newBook.year;
+        }
+
+        books[idBook] = oldBook;
+    }
+
+
+    function deleteBook(uint32 idBook) public{
+        delete books[idBook];
+    }
+
+    // pure not write or read from blockchain
+    function compare(string memory needed, string memory expected)
+        private
+        pure
+        returns (bool)
+    {
+        bytes memory arrNeeded = bytes(needed);
+        bytes memory arrExpected = bytes(expected);
+        return
+            arrNeeded.length == arrExpected.length &&
+            keccak256(arrNeeded) == keccak256(arrExpected);
+    }
+}
+//["1984", 1949]
+
